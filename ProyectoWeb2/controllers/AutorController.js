44e59ed5fbacd6f72eaa2169e546c2ca0316c7d2
FabioSanabria <fabio.sanabria@ucr.ac.cr>
2023-11-04 18:57:45 -0600

    Añadiendo los metodos que puede tener el Autor
    Hay que cambiarles los redirect y los render por lo que tiene Juan en el frontend

---
+++ ProyectoWeb2/controllers/AutorController.js
@@ -0,0 +1,55 @@
+// Suponiendo que estás utilizando Express.js
+const { raw } = require('mysql2');
+const { Autor } = require('../models/autor'); // Importa el modelo de Autor
+
+// Suponiendo que 'Autor' es tu modelo de Sequelize para autores
+
+const obtenerTodosLosAutores = async (req, res) => {
+    const autores = await autor.findAll(    {
+        raw : true
+    }).catch(err => console.log(err));
+    // TODO: Cambiar el nombre de la vista a autores porque no se como se llama el lugar donde va a tener los
+    // autores(Juan)
+    await res.render('autores', {autores});
+}
+
+const obtenerAutorPorId = async (req, res) => {
+    const autor = await autor.findByPk(req.params.id).catch(err => console.log(err));
+    await res.render('autor', {autor});
+}
+
+const crearAutor = async (req, res) => {
+    const autor = await autor.create({
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        contraseña: req.body.contraseña,
+        
+    }).catch(err => console.log(err));
+    await res.redirect('/autores');
+}
+
+const actualizarAutor = async (req, res) => {
+    const autor = await autor.update({
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        contraseña: req.body.contraseña,
+    }, {
+        where: { id: req.params.id }
+    }).catch(err => console.log(err));
+    await res.redirect('/autores');
+}
+
+const eliminarAutor = async (req, res) => {
+    const autor = await autor.destroy({
+        where: { id: req.params.id }
+    }).catch(err => console.log(err));
+    await res.redirect('/autores');
+}
+
+module.exports = {
+    obtenerTodosLosAutores,
+    obtenerAutorPorId,
+    crearAutor,
+    actualizarAutor,
+    eliminarAutor
+};
