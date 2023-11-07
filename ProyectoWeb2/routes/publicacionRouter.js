06d709e91c1b8e61f3a1241a2155644b4e5dbf1e
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2023-11-07 08:15:58 -0600

    Corrigiendo router

--- ProyectoWeb2/routes/publicacionRouter.js
+++ ProyectoWeb2/routes/publicacionRouter.js
@@ -1,32 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const publicacionController = require('../controllers/PublicacionController'); // Import the controller
+const PublicacionController = require('../controllers/publicacionController');
 
-const publicacionController = new PublicacionController(); // Create an instance of the controller
+const publicacionController = new PublicacionController();
 
-// Route to obtain all publications
-router.get('/', async (req, res) => {
-    await publicacionController.obtenerTodasLasPublicaciones(req, res);
-});
+// Route for obtaining all publicaciones
+router.get('/', publicacionController.obtenerTodasLasPublicaciones);
 
-// Route to create a new publication
-router.post('/', async (req, res) => {
-    await publicacionController.crearPublicacion(req, res);
-});
+// Route for creating a new publicacion
+router.post('/', publicacionController.crearPublicacion);
 
-// Route to obtain publications of a specific author
-router.get('/autor/:autorId', async (req, res) => {
-    await publicacionController.obtenerPublicacionesDeUnAutor(req, res);
-});
+// Route for obtaining publicaciones of a specific author
+router.get('/autor/:autorId', publicacionController.obtenerPublicacionesDeUnAutor);
 
-// Route to obtain the most recent publications
-router.get('/recientes', async (req, res) => {
-    await publicacionController.obtenerPublicacionesMasRecientes(req, res);
-});
+// Route for obtaining most recent publicaciones
+router.get('/recientes', publicacionController.obtenerPublicacionesMasRecientes);
 
-// Route to delete publications of a specific author
-router.delete('/autor/:autorId', async (req, res) => {
-    await publicacionController.eliminarPublicacionesDeUnAutor(req, res);
-});
+// Route for deleting publicaciones of a specific author
+router.delete('/autor/:autorId', publicacionController.eliminarPublicacionesDeUnAutor);
 
 module.exports = router;
