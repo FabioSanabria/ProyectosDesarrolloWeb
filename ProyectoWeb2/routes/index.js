032080eab676d230b05c0a612cb2cce4b67af5b2
FabioSanabria <fabio.sanabria@ucr.ac.cr>
2023-11-11 14:17:19 -0600

    Arreglando ruta de index

--- ProyectoWeb2/routes/index.js
+++ ProyectoWeb2/routes/index.js
@@ -3,15 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-   req.session.autorId = 0;
-   req.session.nombre = 'Invitado'
-   req.session.autorTipo = 'Invitado';
+    req.session.autorId = 0;
+    req.session.nombre = 'Invitado';
+    req.session.autorTipo = 'Invitado';
 
-   autorId = req.session.autorId;
-   nombre = req.session.nombre;
-   autorTipo = req.session.autorTipo;
-    res.render('index',
-        { title: 'Diario Web' }, { autorId }, { nombre }, {autorTipo});
+    var autorId = req.session.autorId;
+    var nombre = req.session.nombre;
+    var autorTipo = req.session.autorTipo;
+
+    res.render('index', {
+        title: 'Diario Web',
+        autorId: autorId,
+        nombre: nombre,
+        autorTipo: autorTipo
+    });
 });
 
 module.exports = router;
